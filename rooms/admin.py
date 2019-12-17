eccf3724f8bc56de7b33f8beea0c46b807a2b16b
ji-yeonpark <happpysophiapark@gmail.com>
2019-12-17 20:48:46 +0900

    #6 Room Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,7 +15,68 @@ class RoomAdmin(admin.ModelAdmin):
 
     """Room Admin Definition"""
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules",),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+    ordering = ("name",)
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+    search_fields = ("^city", "^host__username")
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    def count_amenities(self, obj):
+        """
+        self: 부모 클래스
+        object: 현재 행(row)
+        """
+        # print(obj.amenities.all()) -> QuerySet
+        return "P"
+
+    # 필드 명 변경 (변경하면 자동 정렬 안됨.)
+    # count_amenities.short_description = "amenity"
 
 
 @admin.register(models.Photo)
