8d5419717093b64616a1c1438ba08f0fb4bc4329
ji-yeonpark <happpysophiapark@gmail.com>
2019-12-23 16:45:34 +0900

    #11 Class Based Views

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from rooms import views as room_view
 
-_name = "core"
-urlpatterns = [path("", room_view.all_rooms, name="home")]
+app_name = "core"
+urlpatterns = [path("", room_view.HomeView.as_view(), name="home")]
